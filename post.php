d6e31c117e0c49a2f51ceb107c20ac3f7e5bd524
unknown <s3698114@student.rmit.edu.au>
2020-10-27 10:56:04 +1100

    weather not working

--- post.php
+++ post.php
@@ -1,10 +1,52 @@
+<?php
+$apiKey = "91fcaeb0448a162aac7ca280fba67f1a";
+$cityId = "2158177";
+$googleApiUrl = "https://api.openweathermap.org/data/2.5/weather?appid=" . $apiKey ."&q=melbourne";
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_URL, $googleApiUrl);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, CURLOPT_VERBOSE, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$response = curl_exec($ch);
+
+curl_close($ch);
+$data = json_decode($response);
+$currentTime = time();
+?>
 <html>
 <head>
 <!-- calling css file -->
-<link rel="stylesheet" href="query.css"></head>
+<link rel="stylesheet" href="query.css">
+<link rel="stylesheet" href="weather.css">
+</head>
+<!-- weather API, code taken from https://phppot.com/php/forecast-weather-using-openweathermap-with-php/ -->
 
 <body>
 Your enquiry has been sent.<br>
 Thanks <?php echo ($_POST["name"]) ?>!
+
+<!-- calling the weather code -->
+<div class="report-container">
+        <h2><?php echo $data->name; ?> Weather Status</h2>
+        <div class="time">
+            <div><?php echo date("l g:i a", $currentTime); ?></div>
+            <div><?php echo date("jS F, Y",$currentTime); ?></div>
+            <div><?php echo ucwords($data->weather[0]->description); ?></div>
+        </div>
+        <div class="weather-forecast">
+            <img
+                src="http://openweathermap.org/img/w/<?php echo $data->weather[0]->icon; ?>.png"
+                class="weather-icon" /> <?php echo $data->main->temp_max; ?>°C<span
+                class="min-temperature"><?php echo $data->main->temp_min; ?>°C</span>
+        </div>
+        <div class="time">
+            <div>Humidity: <?php echo $data->main->humidity; ?> %</div>
+            <div>Wind: <?php echo $data->wind->speed; ?> km/h</div>
+        </div>
+    </div>
 </body>
 </html>
