590de408b5bb39666d207c7e20a8e7a4dc7c3eb1
unknown <s3698114@student.rmit.edu.au>
2020-10-27 09:09:50 +1100

    Task 2 HTML, css not finished

---
+++ query.php
@@ -0,0 +1,23 @@
+<html>
+<head></head>
+<body>
+<form>
+<h2>Questions? Comments?<br>
+Send us an email.</h2>
+
+<label for="name">Your Name</label><br>
+<input type="text" id="name" name="name"><br>
+
+<label for="email">Your Email</label><br>
+<input type="email" id="email" name="email"><br>
+
+<label for="phone">Phone</label><br>
+<input type="number" id="phone" name="phone"><br>
+
+<label for="message">Your Message</label><br>
+<input type="textarea" id="message" name="message" row="4" cols="50"><br>
+
+<input type="submit" Value="SEND">
+</form>
+</body>
+</html>
